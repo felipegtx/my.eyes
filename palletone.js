138483fd68c3d764fa4a1e5f4dada8bc199b4509
Felipe <felipegtx@users.noreply.github.com>
2016-02-15 20:10:42 -0200

    Create palletone.js

---
+++ palletone.js
@@ -0,0 +1,32 @@
+self.importScripts("/js/libs/quantize.js");
+
+self.addEventListener("message", function (e) {
+
+    if (e.data === "DONE") {
+        self.postMessage("DONE");
+        self.close();
+        return;
+    }
+
+    var colors = [],
+        imageData = e.data.imageData;
+
+    for (var colorData = 0; colorData < imageData.length; colorData += 4) {
+
+        var red = imageData[colorData],
+            green = imageData[colorData + 1],
+            blue = imageData[colorData + 2],
+            alpha = imageData[colorData + 3];
+
+        if (alpha >= 150) {
+            if (!(red > 250 && green > 250 && blue > 250)) {
+                colors.push([red, green, blue]);
+            }
+        }
+    }
+    var cmap = MMCQ.quantize(colors, 5);
+    if (cmap) {
+        self.postMessage({ x: e.data.x, y: e.data.y, palette: cmap.palette()[0] });
+    }
+}, false);
+
